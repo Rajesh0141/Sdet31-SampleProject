ab7e6d796acdbb02545ea6799ff385773edd2bce
Rajesh Bhukya <Rajesh Bhukya@LAPTOP-8S9POBJ4>
2022-03-23 17:31:17 +0530

    second Commit

---
+++ com.crm.Vtiger/src/test/java/com/crm/vtiger/CreateModules/CreateInvoice.java
@@ -0,0 +1,12 @@
+package com.crm.vtiger.CreateModules;
+
+import org.testng.annotations.Test;
+
+public class CreateInvoice {
+	
+	@Test
+	public void invoice() {
+		System.out.println("invoice is created");
+	}
+
+}
