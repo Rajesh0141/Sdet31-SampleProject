170069dd3aaf3d429ac387140978230c5584369c
palla sudarshan <palla sudarshan@DESKTOP-5ICMDD7>
2022-03-24 15:01:17 +0530

    Rajesh i am sudarshan new added script

---
+++ com.crm.Vtiger/src/test/java/com/crm/vtiger/CreateModules/RajeshTest.java
@@ -0,0 +1,28 @@
+package com.crm.vtiger.CreateModules;
+
+import org.testng.annotations.Test;
+
+public class RajeshTest {
+
+	@Test
+	public void raj() {
+		System.out.println("sudarshan created");
+		
+		
+		
+		
+		
+		
+		
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
