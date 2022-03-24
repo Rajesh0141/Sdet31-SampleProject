b03c41fbb6f2438fbd8a2c3e850bd7d5767ed010
User <User@DESKTOP-LNOB5VR>
2022-03-24 17:07:34 +0530

    Lokeswari Updated Login

---
+++ com.crm.Vtiger/bin/src/test/java/com/crm/vtiger/CreateModules/CreateLoginTest.java
@@ -0,0 +1,10 @@
+package com.crm.vtiger.CreateModules;
+
+import org.testng.annotations.Test;
+
+public class CreateLoginTest {
+@Test
+public void Login(){
+	System.out.println("User3 is Created Login");
+}
+}
